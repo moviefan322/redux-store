d24a315b82ab823c93acfe333ef201b0cf33aecf
moviefan322 <philman202@yahoo.com>
2023-04-16 19:07:36 -0400

    updates index.js to use createRoot

--- client/src/index.js
+++ client/src/index.js
@@ -17,16 +17,14 @@ const store = configureStore({
   },
 });
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
