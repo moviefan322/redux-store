5e3676a444467bad6b12256bdb0690b306f9d38d
moviefan322 <philman202@yahoo.com>
2023-04-19 23:02:21 -0400

    everything works

--- server/server.js
+++ server/server.js
@@ -36,9 +36,9 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
   db.once("open", () => {
     app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`.rainbow);
+      console.log(`API server running on port ${PORT}!`.bold.cyan);
       console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`.trap
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`.rainbow
       );
     });
   });
